e4ca450d250be027a4f0e4675963652b7414369d
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-10 22:15:47 +0100

    Best Store

---
+++ src/main/java/com/boosmytools/beststore/services/ProductsRepository.java
@@ -0,0 +1,9 @@
+package com.boosmytools.beststore.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.boosmytools.beststore.models.Product;
+
+/*public interface ProductsRepository extends JpaRepository<Product,Integer>{
+
+}*/
---
+++ src/test/java/com/boosmytools/beststor/BeststorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boosmytools.beststor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeststorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
